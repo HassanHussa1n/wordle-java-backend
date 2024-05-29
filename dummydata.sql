63cfa2c79ebeed5528d75fa8933cfce3b0e4404f
HassanHussa1n <hassanmehmodhussain@gmail.com>
2024-05-29 13:51:21 +0200

    dummy data

--- dummydata.sql
+++ dummydata.sql
@@ -4,13 +4,8 @@ INSERT INTO users (username) VALUES
 ('user3'),
 ('user4'),
 ('user5');
-INSERT INTO games (user_id, score, word, guesses, game_date) VALUES
-(1, 100, 'apple', 5, '2024-05-20 08:30:00'),
-(2, 150, 'banan', 4, '2024-05-20 09:45:00'),
-(3, 200, 'peach', 6, '2024-05-20 11:00:00'),
-(4, 120, 'grape', 7, '2024-05-20 13:15:00'),
-(5, 180, 'mango', 3, '2024-05-20 15:30:00');
-INSERT INTO word_table (id, word) VALUES
+
+INSERT INTO words (word_id, word) VALUES
 (1, 'apple'),
 (2, 'beach'),
 (3, 'crane'),
@@ -26,3 +21,10 @@ INSERT INTO word_table (id, word) VALUES
 (13, 'mango'),
 (14, 'noble'),
 (15, 'oasis');
+
+INSERT INTO games (user_id, score, wordle, guesses, game_date) VALUES
+(1, 100, 'apple', 5, '2024-05-20 08:30:00'),
+(2, 150, 'beach', 4, '2024-05-20 09:45:00'),
+(3, 200, 'crane', 6, '2024-05-20 11:00:00'),
+(4, 120, 'doubt', 7, '2024-05-20 13:15:00'),
+(5, 180, 'mango', 3, '2024-05-20 15:30:00');
